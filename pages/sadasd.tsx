aafed3e51ec636e0a86daa41c6f5d70cca1dc386
jasonhey101 <binglong2206@gmail.com>
2022-07-10 21:11:10 +0700

    added dynamic routing

---
+++ pages/sadasd.tsx
@@ -0,0 +1,90 @@
+import type { NextPage, GetServerSidePropsContext } from "next";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import cookie from "cookie";
+import authMiddleware from "../utils/auth";
+import Router from "next/router";
+
+interface HomeProps {
+  id: number;
+  username: string;
+}
+
+const Home: NextPage<HomeProps> = ({ id, username }) => {
+  const [data, setData] = useState<string>("No Access");
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboard = async () => {
+    await fetch("http://localhost:8000/data", {
+      credentials: "include",
+    })
+      .then((res) => res.text())
+      .then((data) => setData(data))
+      .catch(() => console.log("cannot connect"));
+  };
+
+  const logout = async () => {
+    const res = await fetch("api/logout_api", {
+      method: "DELETE",
+      credentials: "include",
+    });
+
+    Router.push("/login");
+  };
+
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>DASHBOARD</h1>
+        <h2>User: {username}</h2>
+        <h2>Data: {data}</h2>
+        <button onClick={logout}>Logout</button>
+        <div>
+          <h3>Connect to Youtube</h3>
+          <Link href="api/yt_oauth">
+            <button>youtube</button>
+          </Link>
+          <h3>Connect to Instagram</h3>
+          <Link href="api/fb_oauth">
+            <button onClick={() => setLoading(true)}>instagram</button>
+          </Link>
+          <h3>Connect to Facebook</h3>
+          <Link href="api/fb_oauth">
+            <button onClick={() => setLoading(true)}>facebook</button>
+          </Link>
+          <h3>TikTok coming soon....</h3>
+          <Link href="api/fb_oauth">
+            <button disabled={true}>instagram</button>
+          </Link>
+        </div>
+        <div>{loading ? <h1>loading...</h1> : null}</div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
+
+// SSR processes that can pass props to page component, use this to check token
+// This itself is like a server that listens to page request and return the page HTML with props
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  if (!context.req.headers.cookie) {
+    return {
+      redirect: {
+        destination: "/login",
+      },
+    };
+  }
+  const { accessToken, refreshToken } = cookie.parse(
+    context.req.headers.cookie as string
+  );
+  const serverProps = await authMiddleware(accessToken, refreshToken, context);
+  return serverProps;
+};
