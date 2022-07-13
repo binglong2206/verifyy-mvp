7205bd1c48650a811fe4b623acad5a39e44829c1
jasonhey101 <binglong2206@gmail.com>
2022-07-13 21:05:27 +0700

    organized yt query data to be posted

--- pages/api/yt_data.ts
+++ pages/api/yt_data.ts
@@ -6,7 +6,7 @@ import { rawListeners } from "process";
 import axios from "axios";
 import date from "date-and-time";
 
-interface VideoDetails {
+interface PlaylistItem {
   kind: string;
   etag: string;
   id: string;
@@ -14,6 +14,30 @@ interface VideoDetails {
     videoId: string;
     videoPublishedAt: string;
   };
+}
+
+interface VideoStat {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: any;
+  contentDetails: any;
+  statistics: any;
+  player: any;
+}
+
+interface YT_data {
+  subscriber_count: number;
+  view_count: number;
+  upload_count: number;
+  demographics: [string, string, number][];
+  geographics: [string, number][];
+  medias: {
+    title: string;
+    view_count: number;
+    like_count: number;
+    comment_count: number;
+  }[];
 }
 
 export default async function handler(
@@ -41,57 +65,75 @@ export default async function handler(
     .then((r) => r.json())
     .then((json) => json.access_token);
 
-  // Youtube Data API -> Channel Stats
+  // Youtube Data API -> Channel Stats -> sub,view,uplaods
   const channelStats = await fetch(
     `https://youtube.googleapis.com/youtube/v3/channels?mine=true&part=snippet,contentDetails,statistics&access_token=${yt_accessToken}`
   ).then((r) => r.json());
+  const { viewCount, subscriberCount, videoCount } =
+    channelStats.items[0].statistics;
 
   // // Youtube Data API -> Get top 5 videos from 'Uploads' playlist
   const playlistId =
     channelStats.items[0].contentDetails.relatedPlaylists.uploads;
+
   const uploadPlaylist = await fetch(
     `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=5&playlistId=${playlistId}&key=${process.env.YT_API_KEY}`
   ).then((r) => r.json());
-  const videoIdList = <string[]>uploadPlaylist.items.map((e: VideoDetails) => {
+  const videoIdList = <string[]>uploadPlaylist.items.map((e: PlaylistItem) => {
     return e.contentDetails.videoId;
   });
-  console.log("VIDEO ID LIST: ", videoIdList);
 
-  const videoStats = await fetch(
+  const videoStatsList = await fetch(
     `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,player&id=${videoIdList.toString()}&key=${
       process.env.YT_API_KEY
     }`
   ).then((r) => r.json());
-  console.log("VIDEO STATS: ", videoStats);
+
+  const videoObjects = videoStatsList.items.map((e: VideoStat) => {
+    return {
+      title: e.snippet.title,
+      view_count: e.statistics.viewCount,
+      like_count: e.statistics.likeCount,
+      comment_count: e.statistics.commentCount,
+    };
+  });
 
   // Youtube Analytics API -> Demographics (age&gender)
   const dateNow = new Date();
   const formattedDate = date.format(dateNow, "YYYY-MM-DD");
-  const demographicStats = await fetch(
+  const demographics = await fetch(
     `https://youtubeanalytics.googleapis.com/v2/reports?dimensions=ageGroup,gender&startDate=2000-05-01&endDate=${formattedDate}&ids=channel==MINE&metrics=viewerPercentage&sort=gender,ageGroup&access_token=${yt_accessToken}`
   ).then((r) => r.json());
-  console.log("DEMOGRAPHIC: ", demographicStats);
 
   // Youtube Analytics API -> Geographics (Country by views)
-  const geographicStats = await fetch(
+  const geographics = await fetch(
     `https://youtubeanalytics.googleapis.com/v2/reports?dimensions=country&startDate=2000-05-01&endDate=${formattedDate}&ids=channel==MINE&metrics=views,estimatedMinutesWatched,averageViewDuration,averageViewPercentage,subscribersGained&sort=-views&access_token=${yt_accessToken}`
   ).then((r) => r.json());
-  console.log("GEOGRAPHICS: ", geographicStats);
 
   // Organize all data into single object
+  const organized_data: YT_data = {
+    subscriber_count: subscriberCount,
+    view_count: viewCount,
+    upload_count: viewCount,
+    demographics: demographics.rows,
+    geographics: geographics.rows,
+    medias: videoObjects,
+  };
 
   // Post data as json to DB's controller
-  // await fetch("http://localhost:8000/yt", {
-  //   method: "POST",
-  //   credentials: "include",
-  //   headers: {
-  //     "content-type": "application/json",
-  //     // Send this to backend to verify auth header
-  //     Authorization: JSON.stringify(req.cookies),
-  //   },
-  //   body: JSON.stringify({ test: "whatver" }),
-  // }).then(() => res.redirect("/redirect_edit"));
+  await fetch("http://localhost:8000/yt", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "content-type": "application/json",
+      // Send this to backend to verify auth header again
+      Authorization: JSON.stringify(req.cookies),
+    },
+    body: JSON.stringify(organized_data),
+  })
+    .then(() => res.redirect("/redirect_edit"))
+    .catch((e) => console.error(e));
 
   // Redirect to edit, use special url to Router.push back via client so can include cookie
-  res.redirect("/redirect_edit");
+  // res.redirect("/redirect_edit");
 }
