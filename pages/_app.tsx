b84275d7558d10e9540be2834a1749cb534548b6
jason <binglong2206@gmail.com>
2022-07-20 19:03:46 +0700

    added themeprovider from react-jss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useEffect, useRef, useLayoutEffect } from "react";
 import Script from "next/script";
 import COLORS from "../theme/colors";
 import {useThemeStore} from '../state/useThemeStore'
+import { useFullscreen } from 'react-use';
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   const darkModeStatus = useThemeStore(state => state.darkModeStatus);
+  const fullScreenStatus = useThemeStore(state => state.fullScreenStatus);
   const themeStatus: 'dark' | 'light' = darkModeStatus ? 'dark' : 'light';
 
 
@@ -32,6 +35,15 @@ function MyApp({ Component, pageProps }: AppProps) {
 			document.documentElement.removeAttribute('theme');
 		};
 	}, [darkModeStatus]);
+
+  const tempRef = useRef(null);
+  useFullscreen(tempRef ,fullScreenStatus, {
+		onClose: () => useThemeStore.setState({fullScreenStatus: false}),
+	});
+
+  useLayoutEffect(() => {
+    document.body.classList.add('modern-design');
+	});
 
   return (
     <>
